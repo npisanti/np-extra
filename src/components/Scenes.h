1bda62375fd92be7882c267df91763dbd2c672da
Nicola Pisanti <nicola@npisanti.com>
2019-01-25 13:22:06 +0100

    critical fix

--- src/components/Scenes.h
+++ src/components/Scenes.h
@@ -79,7 +79,7 @@ public:
         return 1.0f;
     }
 
-    inline bool notEnding(){ return (state!=Ended); }
+    inline bool notEnding(){ return state<Ending; }
     inline bool isBeginning(){ return state==Beginning; }
     inline bool isLooping(){ return state==Loop; }
     inline bool isEnding(){ return state==Ending; }
