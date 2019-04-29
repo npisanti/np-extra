adabe969295fdab7d6d11080323d5a806777d2d9
Nicola Pisanti <nicola@npisanti.com>
2019-04-29 15:41:34 +0200

    trivial png folder loading routine

---
+++ src/routines/pngfolder.cpp
@@ -0,0 +1,17 @@
+
+#include "pngfolder.h"
+
+void np::load_png_folder( std::string path, std::vector<ofImage> & images ){
+    
+    ofDirectory dir;
+	dir.listDir( path );
+	dir.allowExt("png");
+
+	if( dir.size() ){
+		images.assign(dir.size(), ofImage());
+	}
+
+	for(int i = 0; i < (int)dir.size(); i++){
+		images[i].load(dir.getPath(i));
+	}
+}
---
+++ src/routines/pngfolder.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "ofMain.h"
+
+namespace np {
+
+    void load_png_folder( std::string path, std::vector<ofImage> & images );
+    
+}
